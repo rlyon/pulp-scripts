20875e4aff26fa3bf64e8fd341073fbcbc9caeac
Rob Lyon <nosignsoflifehere@gmail.com>
2015-01-27 08:36:26 -0800

    update sync and create repos

--- libexec/create-all-repos.sh
+++ libexec/create-all-repos.sh
@@ -185,6 +185,130 @@ pulp-admin rpm repo create \
     --gpg-key=/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
 
 ###############################################################################
+# CentOS 5 Base OS (x86_64)
+###############################################################################
+pulp-admin rpm repo create \
+    --repo-id=centos-5-base-x86_64-live \
+    --display-name="CentOS 5 Base x86_64 (Live)" \
+    --description="CentOS 5 Base x86_64 (Live)" \
+    --feed="http://mirror.centos.org/centos-5/5/os/x86_64/" \
+    --serve-http=False \
+    --serve-https=True \
+    --relative-url=/live/centos/5/base/x86_64 \
+    --gpg-key=/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5
+
+pulp-admin rpm repo create \
+    --repo-id=centos-5-base-x86_64-dev \
+    --display-name="CentOS 5 Base x86_64 (Dev)" \
+    --description="CentOS 5 Base x86_64 (Dev)" \
+    --serve-http=False \
+    --serve-https=True \
+    --relative-url=/dev/centos/5/base/x86_64 \
+    --gpg-key=/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5
+
+pulp-admin rpm repo create \
+    --repo-id=centos-5-base-x86_64-prod \
+    --display-name="CentOS 5 Base x86_64 (Prod)" \
+    --description="CentOS 5 Base x86_64 (Prod)" \
+    --serve-http=False \
+    --serve-https=True \
+    --relative-url=/prod/centos/5/base/x86_64 \
+    --gpg-key=/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5
+
+###############################################################################
+# CentOS 5 Updates (x86_64)
+###############################################################################
+pulp-admin rpm repo create \
+    --repo-id=centos-5-updates-x86_64-live \
+    --display-name="CentOS 5 Updates x86_64 (Live)" \
+    --description="CentOS 5 Updates x86_64 (Live)" \
+    --feed="http://vault.centos.org/5/updates/x86_64/" \
+    --serve-http=False \
+    --serve-https=True \
+    --relative-url=/live/centos/5/updates/x86_64 \
+    --gpg-key=/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5
+
+pulp-admin rpm repo create \
+    --repo-id=centos-5-updates-x86_64-dev \
+    --display-name="CentOS 5 Updates x86_64 (Dev)" \
+    --description="CentOS 5 Updates x86_64 (Dev)" \
+    --serve-http=False \
+    --serve-https=True \
+    --relative-url=/dev/centos/5/updates/x86_64 \
+    --gpg-key=/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5
+
+pulp-admin rpm repo create \
+    --repo-id=centos-5-updates-x86_64-prod \
+    --display-name="CentOS 5 Updates x86_64 (Prod)" \
+    --description="CentOS 5 Updates x86_64 (Prod)" \
+    --serve-http=False \
+    --serve-https=True \
+    --relative-url=/prod/centos/5/updates/x86_64 \
+    --gpg-key=/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5
+
+###############################################################################
+# CentOS 5.4 Base OS (x86_64)
+###############################################################################
+pulp-admin rpm repo create \
+    --repo-id=centos-5.4-base-x86_64-live \
+    --display-name="CentOS 5.4 Base x86_64 (Live)" \
+    --description="CentOS 5.4 Base x86_64 (Live)" \
+    --feed="http://vault.centos.org/5.4/os/x86_64/" \
+    --serve-http=False \
+    --serve-https=True \
+    --relative-url=/live/centos/5.4/base/x86_64 \
+    --gpg-key=/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5
+
+pulp-admin rpm repo create \
+    --repo-id=centos-5.4-base-x86_64-dev \
+    --display-name="CentOS 5.4 Base x86_64 (Dev)" \
+    --description="CentOS 5.4 Base x86_64 (Dev)" \
+    --serve-http=False \
+    --serve-https=True \
+    --relative-url=/dev/centos/5.4/base/x86_64 \
+    --gpg-key=/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5
+
+pulp-admin rpm repo create \
+    --repo-id=centos-5.4-base-x86_64-prod \
+    --display-name="CentOS 5.4 Base x86_64 (Prod)" \
+    --description="CentOS 5.4 Base x86_64 (Prod)" \
+    --serve-http=False \
+    --serve-https=True \
+    --relative-url=/prod/centos/5.4/base/x86_64 \
+    --gpg-key=/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5
+
+###############################################################################
+# CentOS 5.4 Updates (x86_64)
+###############################################################################
+pulp-admin rpm repo create \
+    --repo-id=centos-5.4-updates-x86_64-live \
+    --display-name="CentOS 5.4 Updates x86_64 (Live)" \
+    --description="CentOS 5.4 Updates x86_64 (Live)" \
+    --feed="http://vault.centos.org/5.4/updates/x86_64/" \
+    --serve-http=False \
+    --serve-https=True \
+    --relative-url=/live/centos/5.4/updates/x86_64 \
+    --gpg-key=/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5
+
+pulp-admin rpm repo create \
+    --repo-id=centos-5.4-updates-x86_64-dev \
+    --display-name="CentOS 5.4 Updates x86_64 (Dev)" \
+    --description="CentOS 5.4 Updates x86_64 (Dev)" \
+    --serve-http=False \
+    --serve-https=True \
+    --relative-url=/dev/centos/5.4/updates/x86_64 \
+    --gpg-key=/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5
+
+pulp-admin rpm repo create \
+    --repo-id=centos-5.4-updates-x86_64-prod \
+    --display-name="CentOS 5.4 Updates x86_64 (Prod)" \
+    --description="CentOS 5.4 Updates x86_64 (Prod)" \
+    --serve-http=False \
+    --serve-https=True \
+    --relative-url=/prod/centos/5.4/updates/x86_64 \
+    --gpg-key=/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5
+
+###############################################################################
 # RHEL 6 Server (x86_64)
 ###############################################################################
 pulp-admin rpm repo create --repo-id=rhel-6-server-x86_64-live \
